c44b4254a7f19321ce68b2dfcf8ca5e8752f07f1
SINGHAL, SHUBHAM <SHUBHAM.SINGHAL@breadfinancial.com>
2025-03-06 10:54:22 +0530

    WebView messageHandler now working for SIT,  remove https from Info.plist in example app

--- BreadPartnersSDK/Classes/HTMLHandling/UIComponents/BreadFinancialWebViewInterstitial.swift
+++ BreadPartnersSDK/Classes/HTMLHandling/UIComponents/BreadFinancialWebViewInterstitial.swift
@@ -7,20 +7,21 @@ internal class BreadFinancialWebViewInterstitial: NSObject,
     var onPageLoadCompleted: ((Result<URL, Error>) -> Void)?
 
     func createWebView(with url: URL) -> WKWebView {
-        let userScript = WKUserScript(
-            source: jsScript, injectionTime: .atDocumentStart,
-            forMainFrameOnly: false
-        )
 
         let contentController = WKUserContentController()
-        contentController.addUserScript(userScript)
-        contentController.add(self, name: "message")
+        contentController.add(self, name: "messageHandler")
 
         let config = WKWebViewConfiguration()
         config.websiteDataStore = WKWebsiteDataStore.default()
         config.userContentController = contentController
 
         let webView = WKWebView(frame: .zero, configuration: config)
+        if #available(iOS 16.4, *) {
+            webView.isInspectable = true
+        } else {
+            UserDefaults.standard.set(true, forKey: "WebKitDeveloperExtras")
+        }
+
         webView.navigationDelegate = self
 
         Logger().logLoadingURL(url: url)
@@ -47,65 +48,19 @@ internal class BreadFinancialWebViewInterstitial: NSObject,
         _ webView: WKWebView, didFail navigation: WKNavigation!,
         withError error: Error
     ) {
-        print("Error loading page: \(error.localizedDescription)")
         onPageLoadCompleted?(.failure(error))
     }
 
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print(
-            "WebView finished loading, URL: \(webView.url?.absoluteString ?? "")"
-        )
         if let url = webView.url {
             onPageLoadCompleted?(.success(url))
         }
     }
 
-    let jsScript = """
-        setTimeout(function() {
-            try {
-                window.webkit.messageHandlers.message.postMessage(
-                    {
-                        action: {
-                            type: "SAY_HELLO",
-                            payload: {
-                                name: "Sdk",
-                                ack: false
-                            },
-                        },
-                        fmcMessage: true
-                    },
-                    "*"
-                );
-            } catch (err) {
-                console.error('Error sending initial message:', err);
-            }
-
-            window.addEventListener('message', (event) => {
-                try {
-                    window.webkit.messageHandlers.message.postMessage(
-                        {
-                            action: {
-                                type: "message",
-                                payload: {
-                                    name: event.data,
-                                    ack: false
-                                },
-                            },
-                            fmcMessage: true
-                        },
-                        "*"
-                    );
-                } catch (err) {
-                    console.error('Error sending HANDSHAKE message:', err);
-                }
-            });
-        }, 10000);
-        """
-
     func userContentController(
         _ userContentController: WKUserContentController,
         didReceive message: WKScriptMessage
     ) {
-        print("UserContentController: Message: \(message.body)")
+        print("BreadPartnersSDK: WebViewMessage: \(message.body)")
     }
 }
