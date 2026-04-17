0e9b30fd2f0f68e0fa3576f94ee9c52f5ee8bd23
Joncarlos Tavarez <joncarlos.tavarez@breadfinancial.com>
2026-04-16 21:27:33 -0400

    adding back batch prescreen and pay as low as code

--- Example/BreadPartnersSDK/ViewController.swift
+++ Example/BreadPartnersSDK/ViewController.swift
@@ -302,54 +302,54 @@ class ViewController: UIViewController {
     }
     
  
-//    func noActionPlacement(textView: BreadPartnerLinkText, fontSize: Int) {
-//        // Apply font size to the attributed text while preserving HTML formatting
-//        if let attributedText = textView.attributedText {
-//            let mutableAttributedString = NSMutableAttributedString(attributedString: attributedText)
-//            let fullRange = NSRange(location: 0, length: mutableAttributedString.length)
-//            
-//            // Set base text color to black
-//            mutableAttributedString.addAttribute(.foregroundColor, value: UIColor.black, range: fullRange)
-//            
-//            // Update font sizes while preserving traits (bold, italic, superscript, etc.)
-//            mutableAttributedString.enumerateAttributes(in: fullRange, options: []) { attributes, range, _ in
-//                // Check if this is superscript by looking for baseline offset
-//                let baselineOffset = attributes[.baselineOffset] as? NSNumber
-//                let isSuperscript = (baselineOffset?.floatValue ?? 0) > 0
-//                
-//                if let currentFont = attributes[.font] as? UIFont {
-//                    // Use smaller size for superscript, regular size for normal text
-//                    let newSize = CGFloat(fontSize)
-//
-//                    // Preserve font traits (bold, italic)
-//                    let fontDescriptor = currentFont.fontDescriptor
-//                    let newFontDescriptor = fontDescriptor.withSize(newSize)
-//                    let newFont = UIFont(descriptor: newFontDescriptor, size: newSize)
-//                    
-//                    mutableAttributedString.addAttribute(.font, value: newFont, range: range)
-//                } else {
-//                    // If no font attribute, add default font
-//                    let defaultSize = CGFloat(fontSize)
-//                    let defaultFont = UIFont.systemFont(ofSize: defaultSize)
-//                    mutableAttributedString.addAttribute(.font, value: defaultFont, range: range)
-//                }
-//            }
-//            
-//            textView.attributedText = mutableAttributedString
-//        }
-//
-//        NSLayoutConstraint.activate([
-//            textView.centerXAnchor.constraint(
-//                equalTo: self.view.centerXAnchor),
-//            textView.topAnchor.constraint(
-//                equalTo: self.view.topAnchor, constant: 100),
-//            textView.leadingAnchor.constraint(
-//                equalTo: self.view.leadingAnchor, constant: 20),
-//            textView.trailingAnchor.constraint(
-//                equalTo: self.view.trailingAnchor, constant: -20),
-//        ])
-//    }
-//  
+    func noActionPlacement(textView: BreadPartnerLinkText, fontSize: Int) {
+        // Apply font size to the attributed text while preserving HTML formatting
+        if let attributedText = textView.attributedText {
+            let mutableAttributedString = NSMutableAttributedString(attributedString: attributedText)
+            let fullRange = NSRange(location: 0, length: mutableAttributedString.length)
+            
+            // Set base text color to black
+            mutableAttributedString.addAttribute(.foregroundColor, value: UIColor.black, range: fullRange)
+            
+            // Update font sizes while preserving traits (bold, italic, superscript, etc.)
+            mutableAttributedString.enumerateAttributes(in: fullRange, options: []) { attributes, range, _ in
+                // Check if this is superscript by looking for baseline offset
+                let baselineOffset = attributes[.baselineOffset] as? NSNumber
+                let isSuperscript = (baselineOffset?.floatValue ?? 0) > 0
+                
+                if let currentFont = attributes[.font] as? UIFont {
+                    // Use smaller size for superscript, regular size for normal text
+                    let newSize = CGFloat(fontSize)
+
+                    // Preserve font traits (bold, italic)
+                    let fontDescriptor = currentFont.fontDescriptor
+                    let newFontDescriptor = fontDescriptor.withSize(newSize)
+                    let newFont = UIFont(descriptor: newFontDescriptor, size: newSize)
+                    
+                    mutableAttributedString.addAttribute(.font, value: newFont, range: range)
+                } else {
+                    // If no font attribute, add default font
+                    let defaultSize = CGFloat(fontSize)
+                    let defaultFont = UIFont.systemFont(ofSize: defaultSize)
+                    mutableAttributedString.addAttribute(.font, value: defaultFont, range: range)
+                }
+            }
+            
+            textView.attributedText = mutableAttributedString
+        }
+
+        NSLayoutConstraint.activate([
+            textView.centerXAnchor.constraint(
+                equalTo: self.view.centerXAnchor),
+            textView.topAnchor.constraint(
+                equalTo: self.view.topAnchor, constant: 100),
+            textView.leadingAnchor.constraint(
+                equalTo: self.view.leadingAnchor, constant: 20),
+            textView.trailingAnchor.constraint(
+                equalTo: self.view.trailingAnchor, constant: -20),
+        ])
+    }
+  
 
     
     func openExperienceFlow() {
